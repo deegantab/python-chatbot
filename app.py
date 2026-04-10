2a4a55e7817d9d99ed1c45dac741f22921b4a9b3
deegantab <163670593+deegantab@users.noreply.github.com>
2026-04-10 16:10:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO, emit, join_room, leave_room
 import eventlet
