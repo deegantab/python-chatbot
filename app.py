2f3d2fbc059713251fb6542899d3ee76bc7fd855
deegantab <163670593+deegantab@users.noreply.github.com>
2026-04-10 15:15:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO, emit, join_room, leave_room
 import os
+import eventlet
 
 app = Flask(__name__)
 app.secret_key = 'secretkey123'  # For session handling
